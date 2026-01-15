e16e2a687010d7fba4d0a5b6d4bd3007036024c3
annasomos <annasomoss@gmail.com>
2026-01-15 14:09:17 +0100

    feat: add submit button component

---
+++ src/app/SubmitButton.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+export interface SubmitButtonProps {
+  disabled: boolean;
+}
+
+export default function SubmitButton({ disabled }: SubmitButtonProps) {
+  return (
+    <button
+      type="submit"
+      disabled={disabled}
+      className="w-full cursor-pointer text-xl 2xl:text-2xl font-semibold bg-[linear-gradient(90deg,#6793AE_0%,#52A5B8_97.99%)] text-white py-2 xl:py-3 2xl:py-4 disabled:opacity-50 disabled:cursor-default"
+    >
+      Submit
+    </button>
+  );
+}
